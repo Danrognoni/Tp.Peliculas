8a064008db75ded9ca6f6e0c69530292cdb9f5a1
Dante <danrognoni@outlook.es>
2025-05-08 17:42:58 -0300

    controller

--- src/main/java/Controller/PeliculaController.java
+++ src/main/java/Controller/PeliculaController.java
@@ -1,5 +1,40 @@
 package Controller;
 
+import Model.Pelicula;
+import Service.PeliculaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/Peliculas")
 public class PeliculaController {
-    
+
+    private PeliculaService service;
+    @Autowired
+    public PeliculaController(PeliculaService service) {
+        this.service = service;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> crearPelicula(@Valid @RequestBody Pelicula pelicula) {
+        return new ResponseEntity<>(service.crearPelicula(pelicula),HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<?> listarPeliculas(){
+        return ResponseEntity.ok(service.listar());
+    }
+    @GetMapping
+    public ResponseEntity<?> obtenerPorId (@Valid @PathVariable Long Id){
+        return service.buscarPorId(Id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+    @GetMapping
+    public ResponseEntity<?> buscarPoranio (@Valid @PathVariable Integer anio){
+        return ResponseEntity.ok(service.buscarPorAnio(anio));
+    }
+
 }
--- src/main/java/Exception/GlobalExceptionHandler.java
+++ src/main/java/Exception/GlobalExceptionHandler.java
@@ -4,10 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 import java.util.stream.Collectors;
-
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TittleAndDIrectorExist.class)
